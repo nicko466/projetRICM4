152aa06627d8af2ae952bd04a7edab2fcd2c7b65
nicko <nicko466@gmail.com>
2013-03-12 17:09:22 +0100

    read and write quite ok

--- nfcPhoneGap/assets/www/js/read.js
+++ nfcPhoneGap/assets/www/js/read.js
@@ -1,58 +1,55 @@
 function onLoad() {
-    document.addEventListener('deviceready', ready, false);
+	document.addEventListener('deviceready', ready, false);
 }
 
 function clearScreen() {
-    document.getElementById("tagContents").innerHTML = "";
+	document.getElementById("tagContents").innerHTML = "";
 }
 
 function showInstructions() {
-    document.getElementById("tagContents").innerHTML =
-    "<div id='instructions'>" +
-    " <p>Scan a tag to begin.<\/p>" +
-    " <p><\/p>" +    
-    "<\/div>";
+	document.getElementById("tagContents").innerHTML = "<div id='instructions'>" + " <p>Scan a tag to begin.<\/p>" + " <p><\/p>" + "<\/div>";
 }
 
 function template(record) {
-    var recordType = nfc.bytesToString(record.type),
-        payload;
-		// attempt display as a string
-        payload = nfc.bytesToString(record.payload);
-		return payload;
+	var recordType = nfc.bytesToString(record.type), payload;
+	// attempt display as a string
+	payload = nfc.bytesToString(record.payload);
+	return payload;
 }
+
 function parseTag(nfcEvent) {
 	clearScreen();
 	var tag = nfcEvent.tag;
 	var records = tag.ndefMessage;
 	var display = document.getElementById("tagContents");
-    // Display Record Info
-    for (var i = 0; i < records.length; i++) {
-        var record = records[i],
-        p = document.createElement('p');
-        p.innerHTML = nfc.bytesToString(record.payload);
-        display.appendChild(p);
-    }
-    navigator.notification.vibrate(100);
+	// Display Record Info
+	for (var i = 0; i < records.length; i++) {
+		var record = records[i], p = document.createElement('p');
+		p.innerHTML = nfc.bytesToString(record.payload);
+		var typeDeVin = p.getElementsByTagName("typeDeVin"), annee = p.getElementsByTagName("annee"), domaine = p.getElementsByTagName("domaine");
+		p.innerHTML = "Type de vin : " + typeDeVin[0].innerHTML + "<br> annee :" + annee[0].innerHTML + "<br> domaine : " + domaine[0].innerHTML;
+		display.appendChild(p);
+	}
+	navigator.notification.vibrate(100);
+}
+
+function treatment(chaine) {
+
 }
 
 var ready = function() {
-    function failure(reason) {
-        navigator.notification.alert(reason, function() {}, "There was a problem");
-    }
+	function failure(reason) {
+		navigator.notification.alert(reason, function() {
+		}, "There was a problem");
+	}
 
-    nfc.addNdefListener(
-	
-    parseTag,   
 
-    function() {
-       	alert("Success.");
-    }   ,
-    function() {
-        alert("Fail.");
-    }
-);
-    
-    showInstructions();
-    
+	nfc.addNdefListener(parseTag, function() {
+		alert("Success.");
+	}, function() {
+		alert("Fail.");
+	});
+
+	showInstructions();
+
 };
--- nfcPhoneGap/assets/www/js/write.js
+++ nfcPhoneGap/assets/www/js/write.js
@@ -5,26 +5,34 @@ function writeTag(nfcEvent) {
   //var mimeType = "text/pg",//document.forms[0].elements["mimeType"].value,
  //   payload = "Hey i'm mickey",//document.forms[0].elements["payload"].value,
 //    record = ndef.mimeMediaRecord(mimeType, nfc.stringToBytes(payload));
-var ndefMessage = ndef.textRecord("This is a cow","fr");	
+var typeDeVin = document.getElementById("typeDeVin").value,
+	anne = document.getElementById("annee").value,
+	domaine = document.getElementById("domaine").value;
+
+var text = "<typeDeVin>"+typeDeVin+"</typeDeVin><annee>"+annee+"</annee><domaine>"+domaine+"</domaine>";
+var ndefRecord =  ndef.textRecord(text);	
+var ndefMessage = ndef.encodeMessage([ndefRecord]);
   nfc.write(
-        [ndefMessage], 
+        [ndefRecord], 
         function () {
             navigator.notification.vibrate(100);
+            alert(text);
         }, 
         function (reason) {
             navigator.notification.alert(reason, function() {}, "There was a problem");
         }
   );   
+  
 }
 
 var ready = function () {
   
   function win() {
-    alert("Listening for NDEF tags");
+    console.log("Listening for NDEF tags");
   }
   
   function fail() {
-    alert('Failed to register NFC Listener');
+    conole.log('Failed to register NFC Listener');
   }
   nfc.addTagDiscoveredListener(writeTag,win,fail);
 
